dc4dee4614f2c5ae83967aae7579142d54c0ccfc
Juan Bautista Lozano <juanbi.lozano@gmail.com>
2023-11-21 17:44:20 -0300

    agregue json array productos y algunas funciones, cambie estilo pagina principal

---
+++ js/index.js
@@ -0,0 +1,56 @@
+const containerProducts = document.getElementById('container-products');
+const modal = document.getElementById('ventana-modal');
+const carrito = document.getElementById('carrito');
+const totalCarrito = document.getElementById('total');
+const btnClose = document.getElementsByClassName('close')[0];
+const containerCart = document.querySelector('.modal-body');
+const iconMenu = document.getElementById('icon-menu');
+const contenedorProductos = document.querySelector('.contenedor-carrito');
+const cantidadProductos = document.querySelector('.count-products');
+let productosCarrito = [];
+
+class Producto {
+    constructor(imagen, nombre, precio, id) {
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+        this.cantidad = 1;
+        this.subtotal = 0;
+    }
+
+    obtenerTotal() {
+        this.subtotal = this.precio * this.cantidad;
+    }
+}
+
+cargarEventos();
+
+iconMenu.addEventListener('click', showMenu);
+
+function showMenu() {
+    let navBar = document.getElementById('navigation-bar');
+
+    if (navBar.className === 'navigation-bar') {
+        navBar.className += ' responsive';
+    } else {
+        navBar.className = 'navigation-bar';
+    }
+}
+
+const renderizarProductos = () => {
+    productos.forEach((producto) => {
+        const divCard = document.createElement('div');
+        divCard.classList.add('card');
+        divCard.innerHTML = `
+                                <img src="./img/${producto.img}" alt="${producto.nombre}" />
+                                <h4>${producto.nombre}</h4>
+                                <p>$${producto.precio}</p>
+                                <a id="${producto.id}" class="boton agregar-carrito" href="#">Agregar</a>
+                            `;
+
+        containerProducts.append(divCard);
+    });
+};
+
+renderizarProductos();
