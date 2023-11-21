a5df0f04989fefa91b12c3600ec0d5316835ce52
Juan Bautista Lozano <juanbi.lozano@gmail.com>
2023-11-21 18:55:47 -0300

    agruegue la ventana modal del carrito con funcion de abrir y cerrar

--- js/index.js
+++ js/index.js
@@ -22,11 +22,35 @@ class Producto {
     obtenerTotal() {
         this.subtotal = this.precio * this.cantidad;
     }
-}
+};
 
 cargarEventos();
 
-iconMenu.addEventListener('click', showMenu);
+function cargarEventos() {
+    iconMenu.addEventListener('click', showMenu);
+
+    document.addEventListener('DOMContentLoaded', () => {
+        renderizarProductos();
+    });
+
+    carrito.onclick = function () {
+        modal.style.display = 'block';
+    };
+
+    btnClose.onclick = function () {
+        ocultarModal();
+    };
+
+    window.onclick = function () {
+        if (event.target == modal) {
+            ocultarModal();
+        }
+    };
+};
+
+function ocultarModal() {
+    modal.style.display = 'none';
+};
 
 function showMenu() {
     let navBar = document.getElementById('navigation-bar');
@@ -36,17 +60,17 @@ function showMenu() {
     } else {
         navBar.className = 'navigation-bar';
     }
-}
+};
 
 const renderizarProductos = () => {
     productos.forEach((producto) => {
         const divCard = document.createElement('div');
         divCard.classList.add('card');
         divCard.innerHTML = `
-                                <img src="./img/${producto.img}" alt="${producto.nombre}" />
-                                <h4>${producto.nombre}</h4>
-                                <p>$${producto.precio}</p>
-                                <a id="${producto.id}" class="boton agregar-carrito" href="#">Agregar</a>
+                            <img src="./img/${producto.img}" alt="${producto.nombre}" />
+                            <h4>${producto.nombre}</h4>
+                            <p>$${producto.precio}</p>
+                            <a id="${producto.id}" class="boton agregar-carrito" href="#">Agregar</a>
                             `;
 
         containerProducts.append(divCard);
